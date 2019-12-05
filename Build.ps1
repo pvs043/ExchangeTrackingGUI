195399abea96523063bae395b70f29ed1e62b753
Pavel Andreev <pvs043@outlook.com>
2019-12-05 16:23:18 +0500

    Update documentation

--- Build.ps1
+++ Build.ps1
@@ -56,10 +56,10 @@ Description = 'GUI for Get-MessageTrackingLog command'
 PowerShellVersion = '5.1'
 
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-FunctionsToExport = @()
+FunctionsToExport = @('Get-MessageTrackingGUI')
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
-CmdletsToExport = @('Get-MessageTrackingGUI')
+CmdletsToExport = @()
 
 # Variables to export from this module
 VariablesToExport = '*'
